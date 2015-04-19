4272c4f1cbb96a3d5c83736aa44f8eca0a6b552a
Jonathan Soto <jonathansoto.an@gmail.com>
2015-04-18 19:07:57 -0500

    Commit for version 5

---
+++ anim-jonathanSoto.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	$('.playBtn').click(function() {
+		var participants = [];
+		
+		for(var i = 0; i < participantes.length; i += 2) {
+			participants.push({number: participantes[i], name: participantes[i + 1]});
+		}
+		
+		participants = _.shuffle(participants);
+		
+		for(var ind in participants) {
+			$('#name').append('<li>' + participants[ind].name + '</li>');
+			$('#number').append('<li>' + participants[ind].number + '</li>');
+		}
+		
+		var quantityOfLoops = 4;
+		var nameContent = $('#name').children().clone();
+		var numberContent = $('#number').children().clone();
+		
+		for(var currentLoop = 1; currentLoop < quantityOfLoops; ++currentLoop) {
+			$('#name').append(nameContent.clone());
+			$('#number').append(numberContent.clone());
+		}
+		
+		var currentNameLoop = 0;
+		startNameSlotMachine();
+		var currentNumberLoop = 0;
+		startNumberSlotMachine();
+		
+		function startNameSlotMachine() {
+			if(currentNameLoop < quantityOfLoops) {
+				++currentNameLoop;
+				$('#name').animate({
+					top: '-=669px'
+				}, 1500, 'linear', startNameSlotMachine);
+			}
+		}
+		
+		function startNumberSlotMachine() {
+			if(currentNumberLoop < quantityOfLoops) {
+				++currentNumberLoop;
+				$('#number').animate({
+					top: '-=669px'
+				}, 1500, 'linear', startNumberSlotMachine);
+			}
+		}
+	});
+});
---
+++ base-de-datos.js
@@ -0,0 +1,11 @@
+var participantes = [
+	1, 'Juan',
+	2, 'Pedro',
+	3, 'Sofía',
+	4, 'Alberto',
+	5, 'Hugo',
+	6, 'Paco',
+	7, 'Luis',
+	8, 'Andrés',
+	9, 'Charlie'
+];
